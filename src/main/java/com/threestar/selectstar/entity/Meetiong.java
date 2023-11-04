5bb07a0c6ba7adc1d6261a59a952f17a54b7c240
tlagusejr <starc13@naver.com>
2023-11-04 14:35:41 +0900

    깃 초기화

--- src/main/java/com/threestar/selectstar/entity/Meetiong.java
+++ src/main/java/com/threestar/selectstar/entity/Meetiong.java
@@ -1,4 +1,13 @@
 package com.threestar.selectstar.entity;
 
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
 public class Meetiong {
 }
--- src/main/java/com/threestar/selectstar/entity/User.java
+++ src/main/java/com/threestar/selectstar/entity/User.java
@@ -1,4 +1,16 @@
 package com.threestar.selectstar.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
 public class User {
+    @Id
+    @
 }
