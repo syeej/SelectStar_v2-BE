a50783bb2b2f86932b5b7b8f9d0239360e6a8240
gossong <thdwldh99@gmail.com>
2023-11-07 09:10:25 +0900

    meeing, user test code

--- src/main/java/com/threestar/selectstar/repository/MeetingRepository.java
+++ src/main/java/com/threestar/selectstar/repository/MeetingRepository.java
@@ -4,6 +4,8 @@ import com.threestar.selectstar.domain.entity.Meeting;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Date;
@@ -24,6 +26,20 @@ public interface MeetingRepository extends JpaRepository<Meeting,Integer> {
     Page<Meeting> findByDeletedIsAndCreationDateIsGreaterThanEqual(int deleted, Date creationDate, Pageable pageable);
     // 모임글 검색
     List<Meeting> findByTitleLikeAndDeletedIsOrderByCreationDateDesc(String like, int deleted);
+    // 모임글 검색 - 필터 적용
+    // List<Meeting> findByTitleLikeAndDeletedAndCategoryInAndInterestLanguageContainingAndInterestFrameworkContainingAndInterestJobContainingOrderByCreationDateDesc(
+    //     String searchWord, int deleted, List<Integer> categoryList, String searchLanguage, String searchFramework, String searchJob);
+    @Query("SELECT m FROM Meeting m WHERE m.title LIKE %:searchWord% AND m.deleted = :deleted " +
+        "AND m.category IN :categoryList " +
+        "AND (:searchLanguage IS NULL OR m.interestLanguage LIKE CONCAT('%', :searchLanguage, '%')) " +
+        "AND (:searchFramework IS NULL OR m.interestFramework LIKE CONCAT('%', :searchFramework, '%')) " +
+        "AND (:searchJob IS NULL OR m.interestJob LIKE CONCAT('%', :searchJob, '%')) " +
+        "ORDER BY m.creationDate DESC")
+    List<Meeting> findBySearchFilter(@Param("searchWord") String searchWord, @Param("deleted") int deleted,
+        @Param("categoryList") List<Integer> categoryList, @Param("searchLanguage") String searchLanguage,
+        @Param("searchFramework") String searchFramework, @Param("searchJob") String searchJob);
+
+
     // 내가 작성한 글 목록
     List<Meeting> findByUser_UserIdIsAndDeletedIs(Integer user_userId, int deleted);
     // 내가 작성한 글목록 조회(카테고리별)
