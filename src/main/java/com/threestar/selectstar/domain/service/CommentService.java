ab8cff2dc5498268f5222ff6d01e9275b27f128b
tlagusejr <starc13@naver.com>
2023-11-12 20:37:33 +0900

    댓글 서비스 개선

---
+++ src/main/java/com/threestar/selectstar/domain/service/CommentService.java
@@ -0,0 +1,74 @@
+package com.threestar.selectstar.domain.service;
+
+import com.threestar.selectstar.domain.entity.Meeting;
+import com.threestar.selectstar.dto.meeting.request.FindMainPageRequest;
+import com.threestar.selectstar.dto.meeting.response.FindMainPageResponse;
+import com.threestar.selectstar.dto.meeting.response.FindMeetingOneResponse;
+import com.threestar.selectstar.repository.ApplyRepository;
+import com.threestar.selectstar.repository.CommentRepository;
+import com.threestar.selectstar.repository.MeetingRepository;
+import com.threestar.selectstar.repository.UserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+
+    final MeetingRepository meetingRepository;
+    final UserRepository userRepository;
+    final CommentRepository commentRepository;
+    final ApplyRepository applyRepository;
+
+    public CommentService(MeetingRepository meetingRepository, UserRepository userRepository,
+                          CommentRepository commentRepository, ApplyRepository applyRepository) {
+        this.meetingRepository = meetingRepository;
+        this.userRepository = userRepository;
+        this.commentRepository = commentRepository;
+        this.applyRepository = applyRepository;
+    }
+    // 해당 게시글 모든 댓글 조회
+    public Page<FindMainPageResponse> findComment(FindMainPageRequest findMainPageRequest) {
+        // 총 페이지 랑 페이지 리스트 반환
+        Pageable pageable;
+        // 페이징 처리
+        if (findMainPageRequest.getOrder() != null) {
+            pageable = switch (findMainPageRequest.getOrder()) {
+                case "desc" -> PageRequest.of(findMainPageRequest.getPage(),
+                        findMainPageRequest.getSize(),
+                        Sort.by(Sort.Direction.DESC,
+                                findMainPageRequest.getCriteria()));
+                case "asc" -> PageRequest.of(findMainPageRequest.getPage(),
+                        findMainPageRequest.getSize(),
+                        Sort.by(Sort.Direction.ASC,
+                                findMainPageRequest.getCriteria()));
+                default -> PageRequest.of(findMainPageRequest.getPage(),
+                        findMainPageRequest.getSize());
+            };
+        } else {
+            pageable = PageRequest.of(findMainPageRequest.getPage(),
+                    findMainPageRequest.getSize());
+        }
+        Page<Meeting> byDeletedIsOrderByCreationDateDesc;
+        if (findMainPageRequest.getCategory() == null)
+            byDeletedIsOrderByCreationDateDesc = meetingRepository.findByDeletedIs(0,
+                    pageable);
+        else
+            byDeletedIsOrderByCreationDateDesc = meetingRepository.findByDeletedIsAndCategoryIs(0,
+                    findMainPageRequest.getCategory(),
+                    pageable);
+        return byDeletedIsOrderByCreationDateDesc.map(FindMainPageResponse::fromEntity);
+    }
+    public FindMeetingOneResponse findMeetingOne(int meetingId){
+        return meetingRepository.findById(meetingId).
+                map(meeting -> FindMeetingOneResponse.fromEntity(meeting, meeting.getUser().getNickname()))
+                .orElse(null);
+    }
+
+    // 댓글 등록
+
+    // 댓글 삭제
+
+}
